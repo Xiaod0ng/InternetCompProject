e054028a1cd8f61f708c785be2ec5e9c369341b6
xiaodong.yu <yuxiaodongnju@outlook.com>
2022-12-19 16:56:06 +0800

    add start in package.json

--- final-end/dbconfig/db.js
+++ final-end/dbconfig/db.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var mysql = require("mysql");
 const dbConfig = require("./db.config.js");
 
-const serverCa = [fs.readFileSync("../final-end/dbconfig/DigiCertGlobalRootCA.crt.pem", "utf-8")];
+const serverCa = [fs.readFileSync("./final-end/dbconfig/DigiCertGlobalRootCA.crt.pem", "utf-8")]; //node ./final-end/app.js, then use ./certpath, if using cd, then node app.js, need to use ../certpath
 
 var connection = mysql.createPool({
   host: dbConfig.HOST,
